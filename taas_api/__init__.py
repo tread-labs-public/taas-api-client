22281acac693eb93ef57639479ee72e23fa3c928
tiennv147 <tiennv147@gmail.com>
2025-10-17 08:09:08 +0800

    fix: missing to_post_body and add test

--- taas_api/__init__.py
+++ taas_api/__init__.py
@@ -1,9 +1,10 @@
 from taas_api.client import Client
-from taas_api.enums import Strategy
+from taas_api.enums import Strategy, PosSide, OrderStatus, MultiOrderStatus
 from taas_api.data import (
     PlaceOrderRequest,
     PlaceMultiOrderRequest,
     ChildOrder,
     PlaceChainedOrderRequest,
     OrderInChain,
+    SetLeverageRequest,
 )
